5fd8e452c911787109f5b90b4365c3d7911cd31d
7FULL <phgfull@gmail.com>
2023-08-02 23:21:02 +0200

    Initial commit

---
+++ src/main/java/com/plugin/homes/HomesPlugin.java
@@ -0,0 +1,17 @@
+package com.plugin.homes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HomesPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
